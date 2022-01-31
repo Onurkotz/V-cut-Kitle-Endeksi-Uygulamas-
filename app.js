ca7c89fbde7cabc4c6ae9a4fdc20b651c2b57462
Onur Koç <onurkottz@gmail.com>
2022-02-01 00:20:02 +0300

    Hesaplama kodaları eklendi. 1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let formDom = document.querySelector(".form");
 
 formDom.addEventListener("submit", formSubmit);
 
-let scoreCard = ``
+
 
 function formSubmit(event){
     event.preventDefault();
@@ -14,4 +14,37 @@ function formSubmit(event){
     let whindex = weValue/(heValue^2);
     console.log(whindex);
 
+    let scoreCard = document.createElement('li');
+    let show = document.querySelector("#show");
+    scoreCard.classList.add("list-group-item", "mt-3", "list");
+    
+    
+    if ( whindex < 20 ) {
+        scoreCard.innerHTML = `Vücut Kitle Endeksiniz: ${whindex}. ZAYIFSINIZ!`;
+        show.append(scoreCard);
+    } else if ( 20 < whindex < 25 ) {
+        scoreCard.innerHTML = `Vücut Kitle Endeksiniz: ${whindex}. NORMAL`;
+        show.append(scoreCard);
+    } else if ( 25 < whindex < 30 ) {
+        scoreCard.innerHTML = `Bir hekime danışmalısınız: ${whindex}. KİLOLUSUNUZ!`;
+        show.append(scoreCard);
+    } 
+
+    
+
+    
+    
+
+
 }
+
+
+
+/*
+
+
+
+if ( 30 < whindex ) {
+    scoreCard.innerHTML = `Acilen bir uzmandan destek almalısınız: ${whindex}. OBEZ!!!`;
+    show.append(scoreCard);
+}*/
