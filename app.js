84d46d3cca550466e619111b33582d99668f819a
Onur Koç <onurkottz@gmail.com>
2022-02-01 23:06:12 +0300

    Fonksiyonlar tamamlandı.

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
+function clock() {
+    let today = new Date();
+    let hour = today.getHours();
+    let minute = today.getMinutes();
+
+    let clockDom = document.querySelector(".watch");
+
+    let cw = `${hour}.${minute}`;
+
+    clockDom.innerHTML = cw;
+
+    setTimeout(clock, 1000)
+}
+
+clock()
+
 let formDom = document.querySelector(".form");
 
 formDom.addEventListener("submit", formSubmit);
 
-
+let show = document.querySelector("#show");
 
 function formSubmit(event){
     event.preventDefault();
@@ -11,8 +27,13 @@ function formSubmit(event){
     let heValue = document.querySelector("#he").value;
     let weValue = document.querySelector("#we").value;
 
+    if (heValue.length<=0 || weValue<=0){
+        alert("Boş bırakılamaz.");
+        location.reload()
+    }
+
    if (isNaN(heValue) || isNaN(weValue)){
-       alert("Numerik değer giriniz.")
+       alert("Numerik değer giriniz.");      
        location.reload()
    }
 
@@ -21,8 +42,8 @@ function formSubmit(event){
     console.log(whindex);
 
     let scoreCard = document.createElement('li');
-    let show = document.querySelector("#show");
-    scoreCard.classList.add("list-group-item", "mt-3", "list", "mb-5");
+    
+    scoreCard.classList.add("list-group-item", "mt-3", "list",);
 
     
     ( whindex < 25 ) ? scoreCard.innerHTML = `Vücut Kitle Endeksiniz: ${whindex}. NORMAL.` : scoreCard.innerHTML = `Bir hekime danışmalısınız: ${whindex}. KİLOLUSUNUZ!`;
@@ -35,10 +56,20 @@ function formSubmit(event){
         }
     } catch (error){
         alert(error);
+        show.innerHTML = ""
         location.reload()
     };
 
     
 
+}
+
+let reset = document.querySelector(".reset");
+
+reset.addEventListener("click", zero)
+
+function zero(event) {
+    console.log("asdfa")
+    show.innerHTML = ""
 }
 
